0c35eb9f12641e619ae5a81e13c0a33f2be339ed
Nieox <38272073+Nieox@users.noreply.github.com>
2021-04-19 21:18:36 -0700

    added localize_object function

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 import io
 import os
 
-from detect import localize_objects
+
+# [START vision_localize_objects]
+def localize_objects(path):
+    """Localize objects in the local image.
+    Args:
+    path: The path to the local file.
+    """
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
+
+    objects = client.object_localization(
+        image=image).localized_object_annotations
+
+    print('Number of objects found: {}'.format(len(objects)))
+    for object_ in objects:
+        print('\n{} (confidence: {})'.format(object_.name, object_.score))
+        print('Normalized bounding polygon vertices: ')
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(' - ({}, {})'.format(vertex.x, vertex.y))
+# [END vision_localize_objects]
+
 
 
 if __name__ == "__main__":
@@ -9,9 +33,9 @@ if __name__ == "__main__":
     # File directory
     path = os.path.abspath('resources/wakeupcat.jpg')
 
-    # Calls Google API to return JSON coordinate results and identifies objects
+    # Calls Google API to return a response with object confidence rate and their coordinates
     localize_objects(path)
 
     # TODO: Call a library to create a copy of the image and using the JSON response draw onto new image the detected object
 
-    
+
